da003026c5c5e0eb310a9297c1328c6753987a63
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2016-11-13 15:24:10 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ if [ "$ELASTICSEARCH_URL" -o "$ELASTICSEARCH_PORT_9200_TCP" ]; then
         : ${ELASTICSEARCH_URL:='http://elasticsearch:9200'}
         sed -ri "s!^(\#\s*)?(elasticsearch\.url:).*!\2 '$ELASTICSEARCH_URL'!" /opt/kibi/config/kibi.yml
 else
-        "No ES URL parameter, starting local instance... "
+        echo "No ES URL parameter, starting local instance... "
         # Patch demo ES to listen to all interfaces using elasticfence authentication
         echo "network.host: 0.0.0.0" >> /etc/elasticsearch/elasticsearch.yml
         echo "elasticfence.disabled: false" >> /etc/elasticsearch/elasticsearch.yml
