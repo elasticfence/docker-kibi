fa7a9f69e097ecaaa675544a9fd240017a880842
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2016-11-13 14:18:22 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [ "$1" = 'docker-kibi' ]; then
+if [ "$1" = 'docker-elasticfence' ]; then
     if [ "$ELASTICSEARCH_URL" -o "$ELASTICSEARCH_PORT_9200_TCP" ]; then
         : ${ELASTICSEARCH_URL:='http://elasticsearch:9200'}
         sed -ri "s!^(\#\s*)?(elasticsearch\.url:).*!\2 '$ELASTICSEARCH_URL'!" /opt/kibi/config/kibi.yml
