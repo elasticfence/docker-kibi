b078da4e55078243620bee9048e633887f8d84e3
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2016-12-18 19:35:56 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,11 @@
 
 set -e
 
-if [ "$ELASTICSEARCH_URL" -o "$ELASTICSEARCH_PORT_9200_TCP" ]; then
+if [ ! -z "$ELASTICSEARCH_URL" ]; then
         : ${ELASTICSEARCH_URL:='http://elasticsearch:9200'}
         sed -ri "s!^(\#\s*)?(elasticsearch\.url:).*!\2 '$ELASTICSEARCH_URL'!" /opt/kibi/config/kibi.yml
+        # Remove Auth Plugin
+        /opt/kibi/bin/kibi plugin -r kibana-auth-plugin
 else
         echo "No ES URL parameter, starting local instance... "
         # Patch demo ES to listen to all interfaces using elasticfence authentication
@@ -14,15 +16,14 @@ else
         echo "elasticfence.whitelist: [\"127.0.0.1\", \"172.17.0.2\"]" >> /etc/elasticsearch/elasticsearch.yml 
         service elasticsearch start
         sleep 8
+        # create admin user 
+        curl "127.0.0.1:9200/_httpuserauth?mode=adduser&username=admin&password=elasticFence" -u root:elasticFence
+        curl "127.0.0.1:9200/_httpuserauth?mode=updateindex&username=admin&index=/.*" -u root:elasticFence
 fi
 
 # Patch demo kibi to use standard ES port
 perl -p -i -e "s/9220/9200/" /opt/kibi/config/kibi.yml
 perl -p -i -e "s/localhost/0.0.0.0/" /opt/kibi/config/kibi.yml
-
-# create admin user 
-curl "127.0.0.1:9200/_httpuserauth?mode=adduser&username=admin&password=elasticFence" -u root:elasticFence
-curl "127.0.0.1:9200/_httpuserauth?mode=updateindex&username=admin&index=/.*" -u root:elasticFence
 
 # Start Kibi
 /etc/init.d/kibi start
