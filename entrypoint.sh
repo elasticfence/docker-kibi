163c1ac90ffdf8e23bf5aebc6e5acf502f9b68f1
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2017-04-24 00:19:04 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,6 +25,7 @@ fi
 # Patch demo kibi to use standard ES port
 perl -p -i -e "s/9220/9200/" /opt/kibi/config/kibi.yml
 perl -p -i -e "s/localhost/0.0.0.0/" /opt/kibi/config/kibi.yml
+perl -p -i -e "s/default_dashboard_title: Articles//" /opt/kibi/config/kibi.yml
 
 # Start Kibi
 /etc/init.d/kibi start
